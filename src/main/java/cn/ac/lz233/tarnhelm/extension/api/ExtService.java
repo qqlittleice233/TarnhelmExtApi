775dc6a41e4584ebbdc760e98308fe0823cfb18d
lz差不多是条咸鱼了 <lz23333@outlook.com>
2025-03-14 16:22:14 +0800

    refine api method

--- src/main/java/cn/ac/lz233/tarnhelm/extension/api/ExtService.java
+++ src/main/java/cn/ac/lz233/tarnhelm/extension/api/ExtService.java
@@ -1,5 +1,8 @@
 package cn.ac.lz233.tarnhelm.extension.api;
 
+import android.content.Context;
+import android.view.View;
+
 public abstract class ExtService {
 
     private ExtService() {}
@@ -18,13 +21,6 @@ public abstract class ExtService {
     public abstract void onExtInstall();
 
     /**
-     * This method should handle the charSequence and return a processed string.
-     *
-     * @param charSequence The charSequence to be handled by the extension.
-     */
-    public abstract String handleLoadString(CharSequence charSequence);
-
-    /**
      * This method will be called when the extension is uninstalled.
      */
     public abstract void onExtUninstall();
@@ -32,8 +28,20 @@ public abstract class ExtService {
     /**
      * This method will be called when user click the check update button of the extension.
      *
-     * @return The update link of the extension.
+     * @return The direct update link of the extension.
      */
-    public abstract String checkUpdate();
+    public abstract String onCheckUpdate();
 
+    /**
+     * This method should handle the charSequence and return a processed string.
+     *
+     * @param charSequence The charSequence to be handled by the extension.
+     */
+    public abstract String onHandleString(CharSequence charSequence);
+
+    /**
+     * This method should build the configuration panel of the extension by returning a view which may contains widgets that are wrapped in a Layout.
+     *
+     */
+    public abstract View onRequestConfigurationPanel(Context context);
 }
